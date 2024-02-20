66e9b518fe262b5ce62c152519b6e94098cdc67a
Bjorn-23 <bjornandersson84@gmail.com>
2024-02-20 22:10:09 +0100

    Done with DownloadAppPartial

---
+++ Silicon-design-webapp/Models/Sections/DownloadAppViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Silicon_design_webapp.Models.Components;
+
+namespace Silicon_design_webapp.Models.Sections;
+
+public class DownloadAppViewModel
+{
+    public string Id { get; set; } = null!;
+    public ImageViewModel Image { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public List<AppDisplayViewModel> AppDisplay { get; set; } = [];
+}
