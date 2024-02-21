4b5611e2fd3222a7e9bb3649dee4f955f883aa03
Bjorn-23 <bjornandersson84@gmail.com>
2024-02-21 15:53:03 +0100

    added signup rout to auth controller.

---
+++ Silicon-design-webapp/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon_design_webapp.Controllers;
+
+public class AuthController : Controller
+{
+    [Route("/SignUp")]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+}
