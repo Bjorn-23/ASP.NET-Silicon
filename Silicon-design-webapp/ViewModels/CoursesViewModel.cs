99038ff574ef57cc1a13c393082ebf10feeed5a7
Bjorn-23 <bjornandersson84@gmail.com>
2024-02-22 15:14:52 +0100

    Courses page ready

---
+++ Silicon-design-webapp/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Silicon_design_webapp.Models.Components;
+
+namespace Silicon_design_webapp.ViewModels;
+
+public class CoursesViewModel
+{
+    public string Id { get; set; } = null!;
+    public string Title { get; set; } = "Courses";
+    public List<CourseBoxViewModel> Courses { get; set; } = new();
+}
