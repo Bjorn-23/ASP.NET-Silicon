bfd5df7156b3a059123063a0afe9af9e109093e0
Bjorn-23 <bjornandersson84@gmail.com>
2024-02-22 12:26:39 +0100

    sigin styled and working, need to add links for: forgot password and social signin functins.

--- Silicon-design-webapp/Controllers/AuthController.cs
+++ Silicon-design-webapp/Controllers/AuthController.cs
@@ -25,4 +25,25 @@ public class AuthController : Controller
 
         return RedirectToAction("SignIn", "Auth");
     }
+
+    [Route("/signin")]
+    [HttpGet]
+    public IActionResult SignIn()
+    {
+        var viewModel = new SignInViewModel();
+        ViewData["Title"] = viewModel.Title;
+        return View(viewModel);
+    }
+
+    [Route("/signin")]
+    [HttpPost]
+    public IActionResult SignIn(SignInViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
+            return View(viewModel);
+
+        //add logic for creating an account in service here.
+
+        return RedirectToAction("Details", "Account");
+    }
 }
