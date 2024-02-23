40423e3934eb7622d37f7db90a5ecf278e643501
Bjorn-23 <bjornandersson84@gmail.com>
2024-02-23 12:50:35 +0100

    added commentary about refactoring the viewModel

--- Silicon-design-webapp/ViewModels/Sections/Account/AccountSidebarViewModel.cs
+++ Silicon-design-webapp/ViewModels/Sections/Account/AccountSidebarViewModel.cs
@@ -1,11 +1,11 @@
 ﻿using Silicon_design_webapp.Models;
 using Silicon_design_webapp.ViewModels.Components;
-using System.ComponentModel.DataAnnotations;
 
 namespace Silicon_design_webapp.ViewModels.Sections.Account;
 
 public class AccountSidebarViewModel
 {
+    //This should be removed and the picture should instead be its own model that can be got from a database.
     public ImageViewModel ProfileImage { get; set; } = null!;
 
     public AccountDetailsBasicInfoModel AccountInfo { get; set; } = new();
