0edbed3a5a36065f3665f718d27883c0a6594a28
Bjorn-23 <bjornandersson84@gmail.com>
2024-02-23 09:19:40 +0100

    constructor prepared, but commented out as service isnt there yet.

--- Silicon-design-webapp/Controllers/AccountController.cs
+++ Silicon-design-webapp/Controllers/AccountController.cs
@@ -4,5 +4,10 @@ namespace Silicon_design_webapp.Controllers;
 
 public class AccountController : Controller
 {
+    //private readonly AccountService _accountService;
 
+    //public AccountController(AccountService accountService)
+    //{
+    //    _accountService = accountService;
+    //}
 }
