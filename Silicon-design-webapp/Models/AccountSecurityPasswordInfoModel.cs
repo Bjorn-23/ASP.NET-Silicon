5b2ef4e163454f00c784deccc4ce28d7d0e5ab93
Bjorn-23 <bjornandersson84@gmail.com>
2024-02-23 14:33:39 +0100

    account security done with models and viewmodels, needs validation for password and checkbox in controller.

---
+++ Silicon-design-webapp/Models/AccountSecurityPasswordInfoModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Silicon_design_webapp.Models;
+
+public class AccountSecurityPasswordInfoModel
+{
+    [Display(Name = "Current password", Prompt = "********", Order = 0)]
+    public string CurrentPassword { get; set; } = null!;
+
+    [Required(ErrorMessage = "You must enter a new password")]
+    [Display(Name = "New password", Prompt = "********", Order = 1)]
+    [DataType(DataType.Password)]
+    [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$", ErrorMessage = "Password invalid")]
+    public string NewPassword { get; set; } = null!;
+
+    [Required(ErrorMessage = "You must confirm your new password")]
+    [Display(Name = "Confirm new password", Prompt = "Confirm your new password", Order = 2)]
+    [DataType(DataType.Password)]
+    [Compare(nameof(NewPassword), ErrorMessage = "Passwords do not match!")]
+    public string ConfirmNewPassword { get; set; } = null!;
+}
