a8f4c422f2c4fcd0f9a8a1f2467f3bcf10ae3f80
Bjorn-23 <bjornandersson84@gmail.com>
2024-02-27 16:41:32 +0100

    added basic code structure, will await to see how content should be loaded with API.

--- Silicon-design-webapp/Controllers/CoursesController.cs
+++ Silicon-design-webapp/Controllers/CoursesController.cs
@@ -13,4 +13,17 @@ public class CoursesController : Controller
         ViewData["Title"] = viewModel.Title;
         return View(viewModel);
     }
+
+    [Route("/course-details")]
+    [HttpGet]
+    public IActionResult Details(/*string id*/)
+    {
+        //var course = _coursesService.get(id);
+        //ViewData["Title"] = course.Title;
+        //return View(course);
+
+        var viewModel = new CourseDetailsViewModel();
+        ViewData["Title"] = viewModel.Title;
+        return View(viewModel);
+    }
 }
