a8b833936a7731b38a0031045f4bc3d570c76397
Bjorn-23 <bjornandersson84@gmail.com>
2024-02-28 22:45:55 +0100

    created baserepository, started crud.

---
+++ Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Context;
+
+namespace Infrastructure.Repositories;
+
+public class BaseRepository<TEntity>(DataContext context) where TEntity : class
+{
+    private readonly DataContext _context = context;
+
+    public virtual async Task<TEntity> CreateAsync(TEntity entity)
+    {
+        try
+        {
+            _context.Set<TEntity>().Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+        catch { }
+        return null!;
+    }
+
+
+}
