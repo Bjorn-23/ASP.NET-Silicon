62ba7b5e8c85a034b9dd0f8d0277d85ca6f09551
Bjorn-23 <bjornandersson84@gmail.com>
2024-02-28 12:30:55 +0100

    added contact, reformatted css a bit, added select for _forms.scss, restyled ifram to have no border and fixed validation.

---
+++ Silicon-design-webapp/ViewModels/Contact/ContactViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Silicon_design_webapp.Models;
+
+namespace Silicon_design_webapp.ViewModels.Contact;
+
+public class ContactViewModel
+{
+    public string Title { get; set; } = "Contact";
+
+    public ContactModel Form { get; set; } = new();
+
+}
