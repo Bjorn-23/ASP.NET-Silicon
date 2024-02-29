0f076b547b7f64eee234fdd4e4f220c15ba06733
Bjorn-23 <bjornandersson84@gmail.com>
2024-02-29 10:35:45 +0100

    created responsefactory and basrepository

--- Business/Utilities/ResponseResult.cs
+++ Business/Utilities/ResponseResult.cs
@@ -1,16 +1,19 @@
-﻿namespace Business.Utilities;
+﻿//namespace Business.Utilities;
 
-public enum StatusCode
-{
-    OK = 200,
-    ERROR = 400,
-    NOT_FOUND = 404,
-    EXISTS = 409
-}
+//public enum StatusCode
+//{
+//    OK = 200,
+//    ERROR = 400,
+//    NOT_FOUND = 404,
+//    EXISTS = 409
+//}
 
-public class ResponseResult
-{
-    public StatusCode StatusCode { get; set; }
-    public object? ContentResult { get; set; }
-    public string? Message { get; set; }
-}
+//public class ResponseResult
+//{
+//    public StatusCode StatusCode { get; set; }
+//    public object? ContentResult { get; set; }
+//    public string? Message { get; set; }
+//}
+
+
+// cant be accesed from infrastructure layer, moved to infrastructure instead.. DELETE THIS FILE
