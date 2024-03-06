e4388d24d43c2d7d30d76a01fef433adbe497755
Bjorn-23 <bjornandersson84@gmail.com>
2024-03-06 12:27:53 +0100

    removed middleware for error routing and implemented native middleware for error routing.

--- Silicon-design-webapp/Helpers/ApplicationBuilderExtensions.cs
+++ Silicon-design-webapp/Helpers/ApplicationBuilderExtensions.cs
@@ -2,11 +2,6 @@
 
 public static class ApplicationBuilderExtensions
 {
-    public static IApplicationBuilder UseNotFoundMiddleware(this IApplicationBuilder builder)
-    {
-        return builder.UseMiddleware<NotFoundMiddleware>();
-    }
-
     public static IApplicationBuilder UseUserSessionValidationMiddleware(this IApplicationBuilder builder)
     {
         return builder.UseMiddleware<UserSessionValidationMiddleware>();
