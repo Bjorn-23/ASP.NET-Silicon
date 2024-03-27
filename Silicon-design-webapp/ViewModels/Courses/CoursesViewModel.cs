85f7cbd8f328bd9ce47fc93c98f29ace6317d8b7
Bjorn-23 <bjornandersson84@gmail.com>
2024-03-27 13:47:55 +0100

    corrected spelling mistake

--- Silicon-design-webapp/ViewModels/Courses/CoursesViewModel.cs
+++ Silicon-design-webapp/ViewModels/Courses/CoursesViewModel.cs
@@ -8,5 +8,5 @@ public class CoursesViewModel
     public string Title { get; set; } = "Courses";
     public IEnumerable<CourseBoxModel> Courses { get; set; } = [];
     
-    //public IEnumerable<UserBookMarks> Bookmarks { get; set; } = []; // Add yuser bookmarks and loop through them so that they line up with the right Id.
+    //public IEnumerable<UserBookMarks> Bookmarks { get; set; } = []; // Add user bookmarks and loop through them so that they line up with the right Id.
 }
