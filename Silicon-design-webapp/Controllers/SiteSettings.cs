a97308aa0ea8145d7362bae36ecdf984cab9666d
Bjorn-23 <bjornandersson84@gmail.com>
2024-03-28 16:58:15 +0100

    implemented darkmode along with cookie consent and also changed quite a few variables to properly display.

---
+++ Silicon-design-webapp/Controllers/SiteSettings.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon_design_webapp.Controllers
+{
+    public class SiteSettings : Controller
+    {
+        [HttpGet]
+        public IActionResult ChangeTheme(string theme)
+        {
+            var option = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(60),
+            };
+
+            Response.Cookies.Append("ThemeMode", theme, option);
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult CookieConsent()
+        {
+            var option = new CookieOptions
+            {
+                Expires = DateTime.Now.AddYears(1),
+                HttpOnly = true,
+                Secure = true,
+            };
+
+            Response.Cookies.Append("CookieConsent", "true", option);
+            return Ok();
+        }
+    }
+}
