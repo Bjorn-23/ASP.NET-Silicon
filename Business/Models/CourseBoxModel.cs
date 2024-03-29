2fba8fdd41cb7a7b11c14547c15d16953862076d
Bjorn-23 <bjornandersson84@gmail.com>
2024-03-29 13:21:04 +0100

    saving and coming back later, found other changes that need tending to.

--- Business/Models/CourseBoxModel.cs
+++ Business/Models/CourseBoxModel.cs
@@ -16,3 +16,33 @@ public class CourseBoxModel
     public string LengthInHours { get; set; } = null!;
     public string? Rating { get; set; }
 }
+
+///REMAKE MODEL WITH THE BELOW PROPS
+
+//public string Id { get; set; } = null!;
+//public string Title { get; set; } = null!;
+//public string Author { get; set; } = null!;
+//public string ImageUrl { get; set; } = null!;
+//public string AltText { get; set; } = null!;
+
+////public bool Bookmark { get; set; } = false; -- move this to a table in User and add bookmarks as a 
+//public bool isBestSeller { get; set; } = false;
+//public bool isDigital
+//{
+//    get; set, } = false;
+//    public string Currency { get; set; } = null!;
+//public decimal Price { get; set; }
+//public decimal? DiscountPrice { get; set; }
+//public string LengthInHours { get; set; } = null!;
+//public int RatingInPercentage { get; set; }
+//public int NumberOfLikes { get; set; }
+//public int NumberOfRatings { get; set; }
+//public ProgramDetailsModel? Details { get; set; }
+//(string Title, string description)
+//    public string[] WhatYouLearn { get; set; }
+//(string bulletPoint)
+//    public ProgramIncludesModel Includes { get; set; }
+//(icon, string text)
+//    public string CourseDescription(string text)
+//    public string Ingress { get; set; }
+//(string text)
--- Silicon-design-webapp/ViewModels/Courses/CourseDetailsViewModel.cs
+++ Silicon-design-webapp/ViewModels/Courses/CourseDetailsViewModel.cs
@@ -4,8 +4,6 @@ namespace Silicon_design_webapp.ViewModels.Courses;
 
 public class CourseDetailsViewModel()
 {
-    public string Title { get; set; } = null!;
-    public string Ingress { get; set; } = null!;
     public CourseBoxModel Course { get; set; } = new();
 
 }
