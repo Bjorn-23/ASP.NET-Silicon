bcd77b833abb8ed62aabf16870fd4189bba9a491
Bjorn-23 <bjornandersson84@gmail.com>
2024-04-18 14:19:25 +0200

    moved configuration from program.cs to configurations folder in presentation layer.

---
+++ Silicon-design-webapp/Configurations/SessionConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace Silicon_design_webapp.Configurations;
+
+public static class SessionConfiguration
+{
+    public static void RegisterSession(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSession(x =>
+        {
+            x.IdleTimeout = TimeSpan.FromMinutes(20);
+            x.Cookie.IsEssential = true;
+            x.Cookie.HttpOnly = true;
+        });
+    }
+}
