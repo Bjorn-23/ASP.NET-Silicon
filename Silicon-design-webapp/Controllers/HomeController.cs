f1444c5013df05363c4084c5cf87c0bd0fed663c
Bjorn-23 <bjornandersson84@gmail.com>
2024-04-18 08:46:44 +0200

    Fixed try catch in subscription controller.

--- Silicon-design-webapp/Controllers/HomeController.cs
+++ Silicon-design-webapp/Controllers/HomeController.cs
@@ -1,12 +1,9 @@
-﻿using Azure;
-using Business.Models;
+﻿using Business.Models;
 using Infrastructure.Factories;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Silicon_design_webapp.ViewModels.Home;
 using System.Diagnostics;
-using System.Numerics;
 using System.Text;
 
 namespace Silicon_design_webapp.Controllers;
@@ -34,16 +31,17 @@ public class HomeController(IConfiguration configuration) : Controller
     [HttpPost]
     public async Task<IActionResult> Subscribe(SubscribeModel subscribe)
     {
-        if (ModelState.IsValid)
+
+        try
         {
-            try
+            if (ModelState.IsValid)
             {
                 using var http = new HttpClient();
                 var content = new StringContent(JsonConvert.SerializeObject(subscribe), Encoding.UTF8, "application/json");
                 var response = await http.PostAsync($"https://localhost:7034/api/Subscriptions?key={_configuration["ApiKey:Secret"]}", content);
                 if (response.IsSuccessStatusCode)
-                {                
-                    TempData["SubscriptionStatus"] =   response.ReasonPhrase == "Created" ? "Subscription created" : "Subscription updated";
+                {
+                    TempData["SubscriptionStatus"] = response.ReasonPhrase == "Created" ? "Subscription created" : "Subscription updated";
                     return RedirectToAction("Index");
                 }
                 else
@@ -52,16 +50,16 @@ public class HomeController(IConfiguration configuration) : Controller
                     return RedirectToAction("Index");
                 }
             }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-                var error = ResponseFactory.InternalServerError("please contact site owner if problem persists");
-                TempData["SubscriptionStatus"] = $"{error.StatusCode} 500, {error.Message}";
-                return RedirectToAction("Index");
-            }
-        }
 
-        TempData["SubscriptionStatus"] = "Email must be valid";
-        return RedirectToAction("Index");
+            TempData["SubscriptionStatus"] = "Email must be valid";
+            return RedirectToAction("Index");
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            var error = ResponseFactory.InternalServerError("please contact site owner if problem persists");
+            TempData["SubscriptionStatus"] = $"{error.StatusCode} 500, {error.Message}";
+            return RedirectToAction("Index");
+        }
     }
 }
