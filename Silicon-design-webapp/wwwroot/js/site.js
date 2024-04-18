04061b3a78e13b360c948a32f101039990b80e19
Bjorn-23 <bjornandersson84@gmail.com>
2024-04-18 09:37:12 +0200

    added scroll to top function

--- Silicon-design-webapp/wwwroot/js/site.js
+++ Silicon-design-webapp/wwwroot/js/site.js
@@ -14,6 +14,9 @@
 
     ///----- mobile menu ---------------------------------------------
 
+
+    ///----- Dark/Light mode switch ----------------------------------
+
     const sw = document.querySelectorAll('#switchMode')
 
     sw.forEach(sw => {
@@ -28,6 +31,26 @@
                         console.log("Error, please contact site owner if issue persists")
                 })
         })
+    })
+
+    ///----- Dark/Light mode switch ----------------------------------
+
+
+    const scrollButton = document.querySelector("#scrollToTop")
+
+    window.addEventListener('scroll', function () {
+        console.log("eventlistener working")
+        if (window.scrollY >= 600 && scrollButton.classList.contains('hide-btn')) {
+            scrollButton.classList.remove('hide-btn')
+        }
+        if (window.scrollY < 600 && !scrollButton.classList.contains('hide-btn')) {
+ 
+            scrollButton.classList.add('hide-btn')
+        }
+    })
+
+    scrollButton.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
     })
 
 })
